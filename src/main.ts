51e88ec0f1f1a19b7ea19d9d4de0c04c48661080
David Maxwell <david@maxwell.uk.net>
2021-10-14 15:52:36 +0100

    set up LogIn/Out, added Profile Page, Protected Profile Page and configured a 404 page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAuth0Plugin from 'vue-auth0-plugin'
+import { Auth0ClientOptions } from '@auth0/auth0-spa-js'
 
-createApp(App).use(router).mount('#app')
+const auth0Options: Auth0ClientOptions = {
+  // auth0 uses snake case
+  client_id: process.env.VUE_APP_AUTH0_CLIENT_KEY,
+  domain: process.env.VUE_APP_AUTH0_DOMAIN,
+  audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+  redirect_uri: window.location.origin,
+}
+
+const app = createApp(App)
+app.use(VueAuth0Plugin, auth0Options)
+app.provide('auth', app.config.globalProperties.$auth)
+app.use(router)
+app.mount('#app')
